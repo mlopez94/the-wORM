89c2191c372493c5bb8a831a433c42cc0a053e9d
mlopez <lopezmatthew87@gmail.com>
2021-07-15 23:23:16 -0400

    finished associations

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
+// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
+// create connection to our database, pass in your MySQL information for username and password
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
